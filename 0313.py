80d48d9cd0f17266c6f9b2b28379b8ca9c487a76
hayden <hayden.chae@swit.io>
2023-03-13 16:31:06 +0900

    commit

--- 0313.py
+++ 0313.py
@@ -1,1 +1,7 @@
-print('hello world')
+# print(".문자열" " 연결")
+# print("python " + "program" + "\n" + "nana")
+
+# print("hah a " * 3)
+# print('파이썬' * '언어')
+print("원의 원주율: " + '3.14')
+print("python" + 2 * ' programing' + ' haha' * 3)
