84383e9565cd2087dbba3b00d364472500c4c849
hayden <hayden.chae@swit.io>
2023-03-14 13:51:08 +0900

    commit

--- 0313.py
+++ 0313.py
@@ -2,6 +2,16 @@
 # print("python " + "program" + "\n" + "nana")
 
 # print("hah a " * 3)
-# print('파이썬' * '언어')
-print("원의 원주율: " + '3.14')
-print("python" + 2 * ' programing' + ' haha' * 3)
+# # print('파이썬' * '언어')
+# print("원의 원주율: " + '3.14')
+# print("python" + 2 * ' programing' + ' haha' * 3)
+# print('''String Operator + and * are
+# very easy!''')
+
+# print('# 이후는 주석')
+# print('주석')  #주석
+
+print(4 ** (1/2))
+print((2**2)**3) # 4 x 4 x 4 = 64
+print(2 ** (2 ** 3)) # 2 x 2 x 2 x 2 x 2 x 2 x 2 x 2 = 256
+print(2 ** 3)
